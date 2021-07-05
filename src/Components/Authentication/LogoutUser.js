a22c60bda9611671fea5c447fcc2ea6cea12abef
mousedrool <richard.abrahamson@yahoo.com>
2021-07-05 12:18:10 -0500

    added core logout functionality.  this clears and logs out the api session, sets login = false and resets the current page view.

---
+++ src/Components/Authentication/LogoutUser.js
@@ -0,0 +1,26 @@
+const LogoutUser = (props) => {
+
+    const endSessionHandler = (event) => {
+        event.preventDefault();
+
+        const requestOptions = {
+            headers: { 'Content-Type': 'application/json' }
+        };
+
+        fetch('https://my-react.local:3000/v1/logout/?apiToken=' + props.apiToken, requestOptions)
+            .then(response => response.json())
+            .then(data => console.log("DATER: " + JSON.stringify(data)));
+
+        props.logoutHandler();
+    }
+
+    return (
+        <div>
+            <form onSubmit={endSessionHandler}>
+                <button type="submit">Log Out</button>
+            </form>
+        </div>
+    );
+};
+
+export default LogoutUser;
