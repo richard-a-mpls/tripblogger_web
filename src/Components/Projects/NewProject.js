8ef54726a1e496b630aa79fe5f5f49f7cc314b88
mousedrool <richard.abrahamson@yahoo.com>
2021-07-19 14:14:08 -0500

    slight updates and migrated most fields to refs instead of usestate fields.

--- src/Components/Projects/NewProject.js
+++ src/Components/Projects/NewProject.js
@@ -1,14 +1,15 @@
-import React, {useState} from "react";
+import React, {useRef, useState} from "react";
 import {Card} from "react-bootstrap";
 import "../UI/Global.css";
 
 import axios from 'axios'
 
 const NewProject = (props) => {
-    const [summary, setSummary] = useState('');
-    const [description, setDescription] = useState('');
-    const [location, setLocation] = useState('');
-    const [date, setDate] = useState(new Date());
+
+    const summary = useRef();
+    const description = useRef();
+    const location = useRef();
+    const date = useRef();
     const [photoData, setPhotoData] = useState('');
     const [photoSelected, setPhotoSelected] = useState(false);
 
@@ -18,23 +19,24 @@ const NewProject = (props) => {
         const formData = new FormData();
         formData.append('file', photoData);
         axios.post("http://localhost:8080/v1/photos", formData, { // receive two parameter endpoint url ,form data
-            headers: { Authorization: `Bearer ${props.apiToken}` }
+            headers: {Authorization: `Bearer ${props.apiToken}`}
         })
             .then(response => {
                 const imgData = response.data;
                 console.log(imgData.id);
                 const projectDays = [
                     {
-                        'datestmp': date
+                        'datestmp': date.current.value
                     }
                 ]
                 const updateValue = {
-                    'summary': summary,
-                    'description': description,
-                    'location': location,
+                    'summary': summary.current.value,
+                    'description': description.current.value,
+                    'location': location.current.value,
                     'project_days': projectDays,
                     'showcase_photo_id': imgData.id
                 }
+                console.log(updateValue);
 
                 fetch('http://localhost:8080/v1/me/projects', {
                     method: 'post',
@@ -48,24 +50,8 @@ const NewProject = (props) => {
                     .then(data => {
                         props.viewProject(data);
                     });
-
-                setSummary('');
-                setDescription('');
-                setLocation('');
-            })};
-
-    const summaryChangeHandler = (event) => {
-        setSummary(event.target.value);
-    }
-    const descriptionChangeHandler = (event) => {
-        setDescription(event.target.value);
-    }
-    const locationChangeHandler = (event) => {
-        setLocation(event.target.value);
-    }
-    const dateChangeHandler = (event) => {
-        setDate(event.target.value);
-    }
+            });
+    };
 
     const setPhotoDataHandler = (event) => {
         setPhotoData(event.target.files[0])
@@ -85,18 +71,14 @@ const NewProject = (props) => {
                     <div className="bform-control">
                         <form onSubmit={submitProjectHandler}>
                             <Card.Text>
-                                <label>Summary: </label><input type="text" onChange={summaryChangeHandler}
-                                                               value={summary}
+                                <label>Summary: </label><input type="text" ref={summary}
                                                                placeholder="A brief summary of your project"/><br/>
-                                <label>Description: </label><input type="text" onChange={descriptionChangeHandler}
-                                                                   value={description}
+                                <label>Description: </label><input type="text" ref={description}
                                                                    placeholder="A description of your project"/><br/>
-                                <label>Location: </label><input type="text" onChange={locationChangeHandler}
-                                                                value={location}
+                                <label>Location: </label><input type="text" ref={location}
                                                                 placeholder="The location associated with your project"/><br/>
                                 <label>Date: </label>
-                                <input type="date" onChange={dateChangeHandler}
-                                       value={date}
+                                <input type="date" ref={date}
                                        placeholder="The day/first day of your project contents"/><br/>
                                 <label>Photo: </label>
                                 <label for="file" className="inputfile">Choose a file</label>
