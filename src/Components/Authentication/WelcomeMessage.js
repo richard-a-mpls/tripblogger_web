a609432f6bd0e0784580795e1fcab50bb4bd009c
mousedrool <richard.abrahamson@yahoo.com>
2021-07-21 12:05:51 -0500

    changes to use new css and card component.

--- src/Components/Authentication/WelcomeMessage.js
+++ src/Components/Authentication/WelcomeMessage.js
@@ -4,65 +4,55 @@ import {Card} from "react-bootstrap";
 const WelcomeMessage = (props) => {
 
     return (
-        <div className="bform-control">
-            <Card style={{width: 'auto', marginBottom: '15px'}}>
-                <Card.Header>
-                    <h2 className="breadcrumb-text">TripBlogger Dashboard</h2>
-                    <form className='breadcrumb-actions' onSubmit={props.changePageState}>
-                        <button type="submit" className="button">Get Started</button>
-                    </form>
-                </Card.Header>
-                <Card.Body>
-                    <Card.Text>
-                        <p>
-                            Tripblogger is an interactive project builder allowing organization of trip experiences and
-                            photos and is available via the web, android or iOS.
-                        </p>
-                        <p>
-                            To get started, click the get started button to the upper-right.
-                        </p>
-                        <Card style={{width: 'auto', marginBottom: '15px'}}>
-                            <Card.Header>
-                                <h3 className="breadcrumb-text">Connections: Recent Projects</h3>
-                                <form className='breadcrumb-actions'>
-                                    <button type="button" onClick={props.changePageState} className="button">&gt;&gt;</button>
-                                </form>
-                            </Card.Header>
-                            <Card.Body>
-                                <Card.Text>
-                                    <p>PLACEHOLDER: be the first to publish a project.</p>
-                                </Card.Text>
-                            </Card.Body>
-                        </Card>
-                        <Card style={{width: 'auto', marginBottom: '15px'}}>
-                            <Card.Header>
-                                <h3 className="breadcrumb-text">Community: Recent Projects</h3>
-                                <form className='breadcrumb-actions' onSubmit={props.changePageState}>
-                                    <button type="submit" className="button">&gt;&gt;</button>
-                                </form>
-                            </Card.Header>
-                            <Card.Body>
-                                <Card.Text>
-                                    <p>PLACEHOLDER: be the first to publish a project.</p>
-                                </Card.Text>
-                            </Card.Body>
-                        </Card>
-                        <Card style={{width: 'auto', marginBottom: '15px'}}>
-                            <Card.Header>
-                                <h3 className="breadcrumb-text">My Recent Projects</h3>
-                                <form className='breadcrumb-actions' onSubmit={props.changePageState}>
-                                    <button type="submit" className="button">&gt;&gt;</button>
-                                </form>
-                            </Card.Header>
-                            <Card.Body>
-                                <Card.Text>
-                                    <p>PLACEHOLDER: no projects found.</p>
-                                </Card.Text>
-                            </Card.Body>
-                        </Card>
-                    </Card.Text>
-                </Card.Body>
-            </Card>
+        <div className="main-body">
+            <div className="main-body-header">
+                <h3>TripBlogger Dashboard</h3>
+                <form className="wb-form-control" onSubmit={props.changePageState}>
+                    <button type="submit">Get Started</button>
+                </form>
+            </div>
+            <div className="main-body-text">
+                <p>
+                    Tripblogger is an interactive project builder allowing organization of trip experiences and
+                    photos and is available via the web, android or iOS.
+                </p>
+                <p>
+                    To get started, click the get started button to the upper-right.
+                </p>
+                <div className="main-body">
+                    <div className="main-body-header">
+                        <h4>Connections: Recent Projects</h4>
+                        <form className="wb-form-control">
+                            <button type="button" onClick={props.changePageState}>&gt;&gt;</button>
+                        </form>
+                    </div>
+                    <div className="main-body-text">
+                        <p>PLACEHOLDER: be the first to publish a project.</p>
+                    </div>
+                </div>
+                <div className="main-body">
+                    <div className="main-body-header">
+                        <h4>Community: Recent Projects</h4>
+                        <form className="wb-form-control" onSubmit={props.changePageState}>
+                            <button type="submit">&gt;&gt;</button>
+                        </form>
+                    </div>
+                    <div className="main-body-text">
+                        <p>PLACEHOLDER: be the first to publish a project.</p>
+                    </div>
+                </div>
+                <div className="main-body">
+                    <div className="main-body-header">
+                        <h4>My Recent Projects</h4>
+                        <form className="wb-form-control" onSubmit={props.changePageState}>
+                            <button type="submit">&gt;&gt;</button>
+                        </form>
+                    </div>
+                    <div className="main-body-text">
+                        <p>PLACEHOLDER: no projects found.</p>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 }
