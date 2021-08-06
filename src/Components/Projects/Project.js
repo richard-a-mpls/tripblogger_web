5ed0c8b99146d17d4ac479c3eb0cd50257e0f43d
mousedrool <richard.abrahamson@yahoo.com>
2021-08-06 15:27:11 -0500

    move duplicated code to shared component and other refactoring tweaks.

--- src/Components/Projects/Project.js
+++ src/Components/Projects/Project.js
@@ -2,13 +2,14 @@ import ProjectActionButtons from "./ProjectActionButtons";
 import axios from "axios";
 import {useContext} from "react";
 import AuthorizationContext from "../../Context/authorization_context";
+import ShareStatus from "./ShareStatus";
 
 const Project = props => {
     const authCtx = useContext(AuthorizationContext);
 
     let date = new Date();
-    if (props.days !== undefined) {
-        date = new Date(props.days[0].datestmp.split("-"));
+    if (props.datestmp !== undefined) {
+        date = new Date(props.project.datestmp.split("-"));
     }
     const month = date.toLocaleString('en-US', {month: 'long'});
     const day = date.toLocaleString('en-US', {day: '2-digit'});
@@ -16,7 +17,7 @@ const Project = props => {
 
     const deleteHandler = (event) => {
         event.preventDefault();
-        axios.delete('https://my-react.local:3000/v1/me/projects/' + props.id,
+        axios.delete('https://my-react.local:3000/v1/me/projects/' + props.project._id,
             {
                 headers: {
                     'Content-Type': 'application/json',
@@ -24,32 +25,26 @@ const Project = props => {
                 }
             })
             .then(response => console.log(response.data));
-        props.removeProject(props.id);
+        props.removeProject(props.project._id);
     }
 
     return (
         <main>
             <header>
                 <div>
-                    <h4>{props.summary}</h4>
-                    <ProjectActionButtons projectId={props.id} editProjectHandler={props.editProjectHandler}
+                    <h4>{props.project.summary}</h4>
+                    <ProjectActionButtons projectId={props.project._id} editProjectHandler={props.editProjectHandler}
                                           deleteHandler={deleteHandler}/>
                 </div>
             </header>
             <div className="content">
                 <div>
                     <img alt="showcase"
-                         src={"https://my-react.local:3000/v1/photos/" + props.photo_id}/>
+                         src={"https://my-react.local:3000/v1/photos/" + props.project.showcase_photo_id}/>
                     <div className="project-details">
-                        <b>{props.location} - {month} {day} {year}</b><br/>
-                        {props.description}<br/><br/>
-                        <i>
-                            {(props.share_with === "private") && "This project is only viewable by you."}
-                            {(props.share_with === "connections" && props.published) && "Only you and your connections can view this project."}
-                            {(props.share_with === "connections" && !props.published) && "Once published, only you and your connections can view this project."}
-                            {(props.share_with === "public" && props.published) && "This project is viewable by everyone."}
-                            {(props.share_with === "public" && !props.published) && "Once published, this project will be viewable by everyone."}
-                        </i>
+                        <b>{props.project.location} - {month} {day} {year}</b><br/>
+                        {props.project.description}<br/><br/>
+                        <ShareStatus project={props.project}/>
                     </div>
                 </div>
             </div>
