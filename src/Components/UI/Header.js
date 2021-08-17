b9eae8623b2e83e6a371560a159e7e698eff7a1b
mousedrool <richard.abrahamson@yahoo.com>
2021-08-17 12:36:56 -0500

    updates for redux toolkit refactor.

--- src/Components/UI/Header.js
+++ src/Components/UI/Header.js
@@ -1,15 +1,22 @@
 import LogoutUser from "../Authentication/LogoutUser";
-import React, {useContext} from "react";
-import AuthorizationContext from "../../Context/authorization_context";
+import React from "react";
+import {uiActions} from "../../store/ui-slice";
+import {useDispatch, useSelector} from "react-redux";
 
 const Header = () => {
-    const authCtx = useContext(AuthorizationContext);
+
+    const dispatch = useDispatch();
+    const userProfile = useSelector(state => state.authSlice.userProfile);
+
+    const editProfileClickHandler = () => {
+        dispatch(uiActions.changePageState("edit_profile"));
+    }
 
     return (
         <header className="wb-form-control">
             <div className="left-item">
-                <button className="button-profile" onClick={authCtx.editProfileClickHandler}>
-                    <img alt="avatar" src={localStorage.getItem('profileUrl')} className="profile-pic"/>&nbsp;{authCtx.userProfile.profile_name}
+                <button className="button-profile" onClick={editProfileClickHandler}>
+                    <img alt="avatar" src={localStorage.getItem('profileUrl')} className="profile-pic"/>&nbsp;{userProfile.profile_name}
                 </button>
             </div>
             <div className="right-item">
