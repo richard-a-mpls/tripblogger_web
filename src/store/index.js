4e3e64ad240a58440b3fa50ea5946c48094f0834
mousedrool <richard.abrahamson@yahoo.com>
2021-08-17 13:23:32 -0500

    profile updates moved to profile-slice.js

--- src/store/index.js
+++ src/store/index.js
@@ -2,9 +2,10 @@ import {configureStore} from "@reduxjs/toolkit";
 
 import authReducer from './auth-slice';
 import uiReducer from './ui-slice';
+import profileReducer from './profile-slice';
 
 const store = configureStore({
-    reducer: {authSlice: authReducer, uiSlice: uiReducer}
+    reducer: {authSlice: authReducer, uiSlice: uiReducer, profileSlice: profileReducer}
 });
 
 export default store;
