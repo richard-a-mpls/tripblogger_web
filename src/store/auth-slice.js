7bdb7f6745622e4969bb7ea610bd89244f69d4d9
mousedrool <richard.abrahamson@yahoo.com>
2021-08-17 13:21:38 -0500

    move user profile items outside of authentication

--- src/store/auth-slice.js
+++ src/store/auth-slice.js
@@ -1,11 +1,11 @@
 import {createSlice} from "@reduxjs/toolkit";
+import {setupProfile} from "./profile-slice";
 import axios from "axios";
 
 export const STORAGE_APITOKEN = 'apiToken';
 
 const initialState = {
     apiToken: localStorage.getItem(STORAGE_APITOKEN),
-    userProfile: '',
     loggedIn: 'pending'
 };
 
@@ -14,48 +14,45 @@ const authSlice = createSlice({
     initialState,
     reducers: {
         setApiToken(state, action) {
+            localStorage.setItem(STORAGE_APITOKEN, action.payload);
             state.apiToken = action.payload;
         },
         logout(state) {
-            state.userProfile = undefined;
+            localStorage.removeItem(STORAGE_APITOKEN);
             state.loggedIn = false;
         },
-        login(state, action) {
-            state.userProfile = action.payload;
+        login(state) {
             state.loggedIn = true;
-        },
-        updateProfile(state, action) {
-            state.userProfile = action.payload;
         }
     }
 });
 
-export const setupProfile = (apiToken) => {
+export const authorizeSession = () => {
     return async (dispatch) => {
-        try {
-            if (!apiToken) {
-                throw Error('no api token');
+        if (localStorage.getItem(STORAGE_APITOKEN)) {
+            // verify the token by getting user profile
+            try {
+                await dispatch(setupProfile(localStorage.getItem(STORAGE_APITOKEN)));
+                dispatch(authActions.login());
+            } catch (error) {
+                dispatch(authActions.logout());
             }
-
-            axios.get('https://my-react.local:3000/v1/profile', {
-                headers: {Authorization: `Bearer ${apiToken}`}
-            })
-                .then(response => {
-                    console.log(response.data);
-                    if (!response.data) {
-                        throw Error('profile data not returned');
-                    }
-                    dispatch(authActions.login(response.data));
-                    localStorage.setItem(STORAGE_APITOKEN, apiToken);
-                })
-                .catch(error => {
-                    console.log("Error here");
-                    dispatch(authActions.logout());
-                });
-        } catch (error) {
-            console.log("Log it out");
+        } else {
             dispatch(authActions.logout());
         }
+    }
+}
+
+export const authorizeFacebook = (fbAccessToken) => {
+    return async (dispatch) => {
+        await axios.post(
+            "http://localhost:8080/v1/authorize",
+            JSON.stringify({"identity_token": fbAccessToken}),
+            {headers: {'Content-Type': 'application/json'}})
+            .then(response => {
+                console.log(response.data.api_token);
+                dispatch(authActions.setApiToken(response.data.api_token));
+            });
     }
 }
 
