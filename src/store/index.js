81018d4e4cf9d429a5399b4a05b5ad37ddcef3f7
mousedrool <richard.abrahamson@yahoo.com>
2021-08-18 10:25:02 -0500

    add project slice to store.

--- src/store/index.js
+++ src/store/index.js
@@ -3,9 +3,10 @@ import {configureStore} from "@reduxjs/toolkit";
 import authReducer from './auth-slice';
 import uiReducer from './ui-slice';
 import profileReducer from './profile-slice';
+import projectReducer from './project-slice';
 
 const store = configureStore({
-    reducer: {authSlice: authReducer, uiSlice: uiReducer, profileSlice: profileReducer}
+    reducer: {authSlice: authReducer, uiSlice: uiReducer, profileSlice: profileReducer, projectSlice: projectReducer}
 });
 
 export default store;
