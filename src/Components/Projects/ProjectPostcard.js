20340fb323eb8ed4ae86a2bc63a470cc8624e2b9
mousedrool <richard.abrahamson@yahoo.com>
2021-08-24 09:54:36 -0500

    Postcard view of projects as well as css to go with it.

---
+++ src/Components/Projects/ProjectPostcard.js
@@ -0,0 +1,24 @@
+import styles from './ProjectPostcard.module.css';
+import React from "react";
+import DateConverter from "../UI/DateConverter";
+
+
+const ProjectPostcard = (props) => {
+
+    return (<>
+        <div className={styles.postcard} onClick={props.onClick}>
+            <div className={styles.contentleft}>
+                <img className={styles.stamp} alt="logo" src={`/v1/photos/${props.project.showcase_photo_id}`}/>
+            </div>
+            <div className={styles.contentright}>
+                <h6 className={styles.text}>{props.project.summary}</h6>
+                <p className={styles.paragraph}>
+                    {props.project.description}
+                </p>
+                <h6 className={styles.text}>Published: <DateConverter date={props.project.datestmp}/></h6>
+            </div>
+        </div>
+    </>);
+}
+
+export default ProjectPostcard;
