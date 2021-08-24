e2215eae5dc42411e5a3445d09b4b370cd99f9da
mousedrool <richard.abrahamson@yahoo.com>
2021-08-24 09:59:13 -0500

    new common date converter component.

---
+++ src/Components/UI/DateConverter.js
@@ -0,0 +1,14 @@
+const DateConverter = (props) => {
+    let date = new Date();
+    if (props.date !== undefined) {
+        date = new Date(props.date.split("-"));
+    }
+    const month = date.toLocaleString('en-US', {month: 'long'});
+    const day = date.toLocaleString('en-US', {day: '2-digit'});
+    const year = date.getFullYear();
+    const dateDisplay = month + " " + day + " " + year;
+
+    return dateDisplay
+}
+
+export default DateConverter;
