6036a8d94f4f1bdd6aff7f051d20d1eee68e7d83
mousedrool <50158729+mousedrool@users.noreply.github.com>
2021-08-25 08:08:40 -0500

    add welcome page  content and reformat items to new style. (#20)

--- src/Components/Projects/ProjectPostcard.js
+++ src/Components/Projects/ProjectPostcard.js
@@ -1,21 +1,38 @@
 import styles from './ProjectPostcard.module.css';
-import React from "react";
+import React, {useState} from "react";
 import DateConverter from "../UI/DateConverter";
 
 
 const ProjectPostcard = (props) => {
+    const [imagesExpaneded, setImagesExpanded] = useState(false);
+
+    const toggleImagesExpaneded = () => {
+        setImagesExpanded(state => !state);
+    }
 
     return (<>
         <div className={styles.postcard} onClick={props.onClick}>
-            <div className={styles.contentleft}>
+            <div className={styles.contenttop}>
                 <img className={styles.stamp} alt="logo" src={`/v1/photos/${props.project.showcase_photo_id}`}/>
             </div>
-            <div className={styles.contentright}>
+            <div className={styles.contentbottom}>
                 <h6 className={styles.text}>{props.project.summary}</h6>
                 <p className={styles.paragraph}>
-                    {props.project.description}
+                    {props.project.description}<br/>
+                    <h6><DateConverter date={props.project.datestmp}/></h6>
                 </p>
-                <h6 className={styles.text}><DateConverter date={props.project.datestmp}/></h6>
+            </div>
+            {imagesExpaneded && <div className={styles.imagegroup}>
+                {props.project.photo_array.map((imageId) =>
+                    <img className={styles.projectimage} alt={imageId} key={imageId}
+                         src={`https://my-react.local:3000/v1/photos/${imageId}`}/>
+                )}
+            </div>}
+            <div onClick={toggleImagesExpaneded} className={styles.expandbar}>
+                {props.project.photo_array.map((imageId) =>
+                    <img className={styles.smallimg} alt={imageId} key={imageId}
+                         src={`https://my-react.local:3000/v1/photos/${imageId}`}/>
+                )}
             </div>
         </div>
     </>);
