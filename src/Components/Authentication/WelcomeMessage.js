c638eacb663129aab0d0dffeea7a4a31d0ea0eb6
mousedrool <richard.abrahamson@yahoo.com>
2021-08-26 15:06:29 -0500

    refactor to consolidated layout of project postcards.

--- src/Components/Authentication/WelcomeMessage.js
+++ src/Components/Authentication/WelcomeMessage.js
@@ -1,43 +1,20 @@
-import React, {useState} from "react";
+import React from "react";
 import PublicProjectsList from "../Projects/PublicProjectsList";
-import MyProjectsList from "../Projects/MyProjectsList";
-import ConnectionsProjectsList from "../Projects/ConnectionsProjectsList";
 import {useSelector} from "react-redux";
 
 const WelcomeMessage = (props) => {
 
-    const [showConnectionsProjects, setShowConnectionsProjects] = useState(false);
     const userProfile = useSelector(state => state.profileSlice.userProfile);
 
     return (
-        <>
-            {showConnectionsProjects &&
-            <main className="center">
-                <header>
-                    <h4>Connections: Recent Projects</h4>
-                    <form className="wb-form-control">
-                        <button type="button" onClick={props.changePageState}>&gt;&gt;</button>
-                    </form>
-                </header>
-                <ConnectionsProjectsList/>
-            </main>
-            }
+        <div style={{marginTop: "60px"}}>
             <main className="center">
                 <button className="fullwidth" onClick={props.changePageState}>
                     Manage or create new Projects, {userProfile.profile_name}?
                 </button>
                 <PublicProjectsList/>
             </main>
-            <main className="center">
-                <header>
-                    <h4>My Projects</h4>
-                    <form className="wb-form-control" onSubmit={props.changePageState}>
-                        <button type="submit">&gt;&gt;</button>
-                    </form>
-                </header>
-                <MyProjectsList/>
-            </main>
-        </>
+        </div>
     );
 }
 
