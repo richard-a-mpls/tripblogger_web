77c0d3d93d28715c2b7c8154032dee02740832c1
mousedrool <50158729+mousedrool@users.noreply.github.com>
2021-08-27 14:38:59 -0500

    Public profiles (#22)
    * updated to show public profile image and name for shared public projects.
    
    * remove duplicated line

--- src/Components/UI/ProfileImage.js
+++ src/Components/UI/ProfileImage.js
@@ -3,12 +3,9 @@ import {useSelector} from "react-redux";
 
 const ProfileImage = (props) => {
     const userProfile = useSelector(state => state.profileSlice.userProfile);
-    console.log(userProfile.profile_img);
     return (<>
             {userProfile && userProfile.profile_img &&
             <img alt="avatar" src={`/v1/photos/${userProfile.profile_img}`} className={props.className}/>}
-            {!userProfile && !userProfile.profile_img &&
-            <img alt="avatar" src="/v1/photos/61215421878dc8e7d6c3b217" className={props.className}/>}
         </>);
 }
 
