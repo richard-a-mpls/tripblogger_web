365e3635adaefd1e7aa438fcb1e0fcf771b84029
mousedrool <richard.abrahamson@yahoo.com>
2021-08-27 14:38:07 -0500

    updated to show public profile image and name for shared public projects.

--- src/Components/Projects/ProjectPostcard.js
+++ src/Components/Projects/ProjectPostcard.js
@@ -1,13 +1,15 @@
 import styles from './ProjectPostcard.module.css';
-import React, {useState} from "react";
+import React, {useEffect, useState} from "react";
 import {useSelector} from "react-redux";
 import DateConverter from "../UI/DateConverter";
 import {useIsAuthenticated} from "@azure/msal-react";
 import ProfileImage from "../UI/ProfileImage";
-
+import axios from "axios";
+import {STORAGE_APITOKEN} from "../../store/auth-slice";
 
 const ProjectPostcard = (props) => {
     const [imagesExpaneded, setImagesExpanded] = useState(false);
+    const [ownerInfo, setOwnerInfo] = useState({});
     const profile = useSelector(state => state.profileSlice.userProfile)
     const isAuthenticated = useIsAuthenticated();
 
@@ -15,27 +17,48 @@ const ProjectPostcard = (props) => {
         setImagesExpanded(state => !state);
     }
 
-    let ownerProfile = <i className="fas fa-users" style={{marginRight: "5px"}}/>;
-    if (profile._id === props.project.profile_id) {
-        ownerProfile = <ProfileImage className="profilePicSm"/>
+    const isOwner = profile._id === props.project.profile_id;
+
+    useEffect(() => {
+        console.log('useEffect');
+        if (!isOwner) {
+            axios.get('/v1/projects/' + props.project._id + '/profile',
+                {headers: {Authorization: `Bearer ${localStorage.getItem(STORAGE_APITOKEN)}`}})
+                .then((response) => {
+                    console.log(response.data);
+                    setOwnerInfo(response.data);
+                })
+        }
+    }, [isOwner, props.project._id]);
+
+    let ownerProfile = <></>;
+    if (isOwner) {
+        ownerProfile = <><ProfileImage className="profilePicSm"/>Your Project<br/></>
+    } else {
+        ownerProfile = <>
+            <img alt="avatar" src={`/v1/photos/${ownerInfo.profile_img}`} className="profilePicSm"/>
+            {ownerInfo.profile_name}<br/>
+        </>
     }
 
     return (<>
         <div className={styles.postcard} onClick={props.onClick}>
+            {isAuthenticated &&
             <div className={styles.headertitle}>
                 <h6 className={styles.headertitle}>
                     {isAuthenticated && ownerProfile}
-                    <DateConverter date={props.project.datestmp}/>
                 </h6>
             </div>
+            }
             <div className={styles.contenttop}>
                 <img className={styles.stamp} alt="logo" src={`/v1/photos/${props.project.showcase_photo_id}`}/>
             </div>
             <div className={styles.contentbottom}>
                 <h6 className={styles.text}>{props.project.summary}</h6>
                 <p className={styles.paragraph}>
-                    {props.project.description}<br/>
+                    {props.project.description} <DateConverter date={props.project.datestmp}/>
                 </p>
+
             </div>
             {imagesExpaneded && <div className={styles.imagegroup}>
                 {props.project.photo_array.map((imageId) =>
