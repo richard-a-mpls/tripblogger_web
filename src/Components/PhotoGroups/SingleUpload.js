98848bc840823230cd1e97b4a34d0d983f9dc327
mousedrool <richard.abrahamson@yahoo.com>
2021-08-27 11:17:06 -0500

    refactor to pull out image upload to it's own component.

---
+++ src/Components/PhotoGroups/SingleUpload.js
@@ -0,0 +1,62 @@
+import FormData from "form-data";
+import axios from "axios";
+import {STORAGE_APITOKEN} from "../../Context/authorization_context";
+import React, {useState} from "react";
+
+const SingleUpload = (props) => {
+
+    const [tmpPhotoId, setTmpPhotoId] = useState();
+    const [uploadingTmpPhoto, setUploadingTmpPhoto] = useState(false);
+
+    const setPhotoDataHandler = (event) => {
+        setUploadingTmpPhoto(true);
+        const formData = new FormData();
+        formData.append('file', event.target.files[0]);
+        axios.post('http://localhost:8080/v1/photos', formData,
+            {
+                headers: {
+                    'Content-Type': "multipart/form-data; boundary=--------------------------a string of numbers that is never the same",
+                    'Authorization': 'Bearer ' + localStorage.getItem(STORAGE_APITOKEN)
+                }
+            })
+            .then(response => {
+                props.updateData(response.data.id);
+                setTmpPhotoId(response.data.id);
+                setUploadingTmpPhoto(false);
+            });
+    }
+
+    return (<>
+        {tmpPhotoId &&
+        <img alt="showcase"
+             src={"https://my-react.local:3000/v1/photos/" + tmpPhotoId}/>
+        }
+
+        {!uploadingTmpPhoto && !tmpPhotoId && !props.photoId &&
+        <div className="wb-form-control addphoto" style={{
+            marginTop: '5px',
+            display: "inline-block",
+            textAlign: "center",
+            backgroundColor: "#26567b",
+            borderRadius: '16px'
+        }}>
+            <label htmlFor="file" className="inputfile">
+                <span style={{display: "block"}}><i className="showcase fas fa-image"/></span>
+                <span style={{display: "block", color: "#ffffff"}}>+ Add Photo</span>
+            </label>
+            <input id="file" className="inputfile" type="file"
+                   onChange={setPhotoDataHandler}/>
+        </div>}
+        {uploadingTmpPhoto &&
+        <img alt="showcase"
+             src={"/spin.gif"}/>
+        }
+        {!tmpPhotoId && props.photoId &&
+        <img alt="showcase"
+             src={"https://my-react.local:3000/v1/photos/" + props.photoId}/>
+        }
+    </>);
+
+}
+
+export default SingleUpload;
