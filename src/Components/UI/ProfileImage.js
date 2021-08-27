d27654060d4d02affe0a6e8c7b37275c7a8374d5
mousedrool <richard.abrahamson@yahoo.com>
2021-08-27 11:50:10 -0500

    use the profile image instead of hard coded.

--- src/Components/UI/ProfileImage.js
+++ src/Components/UI/ProfileImage.js
@@ -1,10 +1,13 @@
 import React from "react";
+import {useSelector} from "react-redux";
 
 const ProfileImage = (props) => {
+    const userProfile = useSelector(state => state.profileSlice.userProfile);
+    console.log(userProfile.profile_img);
     return (<>
-            {localStorage.getItem('profileUrl') &&
-            <img alt="avatar" src={localStorage.getItem('profileUrl')} className={props.className}/>}
-            {!localStorage.getItem('profileUrl') &&
+            {userProfile && userProfile.profile_img &&
+            <img alt="avatar" src={`/v1/photos/${userProfile.profile_img}`} className={props.className}/>}
+            {!userProfile && !userProfile.profile_img &&
             <img alt="avatar" src="/v1/photos/61215421878dc8e7d6c3b217" className={props.className}/>}
         </>);
 }
