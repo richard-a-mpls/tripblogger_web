e71f9f8494ba6842637a72bf87f0421c5a4aa78c
mousedrool <richard.abrahamson@yahoo.com>
2021-09-16 13:23:58 -0500

    update the proxy config settings based on env var

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -1,9 +1,18 @@
 const {createProxyMiddleware} = require('http-proxy-middleware')
 
 module.exports = function(app) {
-    app.use('/v1/me/projects/*/photos', createProxyMiddleware({ target: 'http://localhost:8081/' }))
-    app.use('/v1/projects/*/profile', createProxyMiddleware({ target: 'http://localhost:8081/' }))
-    app.use('/v1/projects/', createProxyMiddleware({ target: 'http://localhost:8080/' }))
-    app.use('/v1/me/', createProxyMiddleware({ target: 'http://localhost:8080/' }))
-    app.use('/v1/', createProxyMiddleware({ target: 'http://localhost:8081/' }))
+
+    if ("PRODUCTION" === process.env.REACT_APP_ENVIRONMENT) {
+        app.use('/v1/me/projects/*/photos', createProxyMiddleware({target: 'http://localhost:8081/'}))
+        app.use('/v1/projects/*/profile', createProxyMiddleware({target: 'http://localhost:8081/'}))
+        app.use('/v1/projects/', createProxyMiddleware({target: 'https://tripblogger-api-spring.azurewebsites.net/', changeOrigin: true}))
+        app.use('/v1/me/', createProxyMiddleware({target: 'https://tripblogger-api-spring.azurewebsites.net/', changeOrigin: true}))
+        app.use('/v1/', createProxyMiddleware({target: 'http://localhost:8081/'}))
+    } else {
+        app.use('/v1/me/projects/*/photos', createProxyMiddleware({target: 'http://localhost:8081/'}))
+        app.use('/v1/projects/*/profile', createProxyMiddleware({target: 'http://localhost:8081/'}))
+        app.use('/v1/projects/', createProxyMiddleware({target: 'http://localhost:8080/'}))
+        app.use('/v1/me/', createProxyMiddleware({target: 'http://localhost:8080/'}))
+        app.use('/v1/', createProxyMiddleware({target: 'http://localhost:8081/'}))
+    }
 }
