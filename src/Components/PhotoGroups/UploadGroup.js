38df366480e58186ab1752a08df1b5ab2659f210
mousedrool <richard.abrahamson@yahoo.com>
2021-09-21 18:34:24 -0500

    refactor styles and location of those styles

--- src/Components/PhotoGroups/UploadGroup.js
+++ src/Components/PhotoGroups/UploadGroup.js
@@ -1,6 +1,8 @@
 import React, {useState} from "react";
 import ImageUpload from "./ImageUpload";
 
+import classes from './PhotoGroups.module.css';
+
 const UploadGroup = (props) => {
 
     const [imageData, setImageData] = useState([]);
@@ -15,33 +17,23 @@ const UploadGroup = (props) => {
     }
 
     return (
-        <div className="wb-form-control">
+        <>
             {imageData.map((data) =>
-                <div className="addphoto"
-                     style={{paddingBottom: "5px", paddingRight: "5px", height: "155px", verticalAlign: "middle"}}>
+                <div className={classes.singleupload}>
                     <ImageUpload key={data.key} projectId={props.projectId} data={data.data}/>
                 </div>
             )}
-            <div className="addphoto" style={{
-                display: "inline-block",
-                textAlign: "center",
-                backgroundColor: "#26567b",
-                borderRadius: '16px',
-                height: "150px",
-                paddingRight: "5px",
-                paddingBottom: "5px",
-                verticalAlign: "middle"
-            }}>
-                <label htmlFor="file" className="inputfile">
-                    <span style={{display: "block"}}><i className="showcase fas fa-image"/></span>
-                    <span style={{display: "block", color: "#ffffff"}}>+ Add Photos</span>
+            <div className={classes.singleupload}>
+                <label htmlFor="file" className={classes.inputfile}>
+                    <span><i className={`${classes.showcase} fas fa-image`}/></span>
+                    <span>+ Add Photos</span>
                 </label>
-                <input id="file" className="inputfile" type="file" multiple="multiple"
+                <input id="file" style={{display: "none"}} type="file" multiple="multiple"
                        onChange={setPhotoDataHandler}/>
 
             </div>
 
-        </div>
+        </>
     );
 }
 export default UploadGroup;
