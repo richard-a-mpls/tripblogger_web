25a95f5b51345d74891490847987e9c150725e65
mousedrool <richard.abrahamson@yahoo.com>
2021-09-21 19:20:48 -0500

    refactor styles for consistency.

--- src/Components/UI/Spinner.js
+++ src/Components/UI/Spinner.js
@@ -1,14 +1,12 @@
-
 import classes from './Spinner.module.css';
 
 const Spinner = () => {
     return (
-        <div className={classes.spinner}>
-            <i className="fullspinner fas fa-cog fa-spin"/>
-        </div>
-    // <div style={{marginTop: "100px", width: "100%", textAlign: "center", display: "block"}}>
-    //     <i className="fullspinner fas fa-cog fa-spin"/>
-    // </div>
+        <main>
+            <div className={classes.spinner}>
+                <i className="fullspinner fas fa-cog fa-spin"/>
+            </div>
+        </main>
     );
 }
 
