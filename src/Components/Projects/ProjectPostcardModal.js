d898fa2cda68c02b52392ca93d7cbc6460bc8f2c
mousedrool <richard.abrahamson@yahoo.com>
2021-10-12 11:48:36 -0500

    adjust styling and navigation for images displayed in modal.

--- src/Components/Projects/ProjectPostcardModal.js
+++ src/Components/Projects/ProjectPostcardModal.js
@@ -7,34 +7,49 @@ const ProjectPostcardModal = (props) => {
     const photoArray = props.photos;
 
     const [photoIndex, setPhotoIndex] = useState(-1);
+    const [photoUrl, setPhotoUrl] = useState(`${photosEndpoint}/${props.showcasePhoto}`);
 
     const nextPhoto = () => {
-        if (photoIndex + 1 < photoArray.length) {
-            setPhotoIndex(photoIndex + 1);
-        } else {
-            setPhotoIndex(-1);
-        }
+        swapPhoto(1);
     }
 
     const prevPhoto = () => {
-        if (photoIndex - 1 < -1) {
-            setPhotoIndex(photoArray.length - 1);
+        swapPhoto(-1);
+    }
+
+    const swapPhoto = (increment) => {
+        let idx = photoIndex;
+        if (idx + increment < -1) {
+            idx = photoArray.length - 1;
+        } else if (idx + increment >= photoArray.length) {
+            idx = -1;
         } else {
-            setPhotoIndex(photoIndex - 1);
+            idx += increment;
+        }
+        setPhotoIndex(idx);
+        resolvePhotoUrl(idx);
+    }
+
+    const resolvePhotoUrl = (idx) => {
+        if (idx === -1) {
+            setPhotoUrl(`${photosEndpoint}/${props.showcasePhoto}`);
+        } else {
+            setPhotoUrl(`${photosEndpoint}/${photoArray[idx]}`);
         }
     }
 
 
     return (
         <Modal onClose={props.closeHandler}>
-            <div style={{textAlign: "center", paddingTop: "10px"}}>
-                <i onClick={props.closeHandler} className={`${classes.modalnav} ${classes.closemodal} fas fa-times`}/>
-                <i onClick={nextPhoto} className={`${classes.modalnav} ${classes.moveright} fas fa-arrow-right`}/>
-                <i onClick={prevPhoto} className={`${classes.modalnav} ${classes.moveleft} fas fa-arrow-left`}/>
-                {photoIndex >= 0 && <img onClick={nextPhoto} className={classes.large} alt={photoIndex}
-                     src={`${photosEndpoint}/${photoArray[photoIndex]}`}/>}
-                {photoIndex === -1 && <img onClick={nextPhoto} className={classes.large} alt={photoIndex}
-                                        src={`${photosEndpoint}/${props.showcasePhoto}`}/>}
+            <div style={{textAlign: "center"}}>
+
+                <i onClick={prevPhoto} className={`${classes.modalnav} fas fa-arrow-left`}/>
+                <i onClick={props.closeHandler} className={`${classes.modalnav} fas fa-times`}/>
+                <i onClick={nextPhoto} className={`${classes.modalnav} fas fa-arrow-right`}/>
+
+                <img onClick={nextPhoto} className={classes.large} alt={photoIndex}
+                     src={photoUrl}/>
+
             </div>
         </Modal>
     );
